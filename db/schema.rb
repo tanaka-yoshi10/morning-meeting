8d95a910b9112d9e51afb370669de072aac5a013
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2016-10-01 17:04:46 +0900

    ユーザー名を設定可能にする

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161001050814) do
+ActiveRecord::Schema.define(version: 20161001074518) do
 
   create_table "notes", force: :cascade do |t|
     t.date     "date"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20161001050814) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
