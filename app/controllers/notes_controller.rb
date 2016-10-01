efb03f41790270e80623c0d3802af40c4efbe822
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2016-10-02 06:24:07 +0900

    JSONのviewを削除

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,78 +3,62 @@ class NotesController < ApplicationController
   before_action :set_user
   before_action :set_note, only: [:show, :edit, :update, :destroy]
 
-  # GET /notes
-  # GET /notes.json
   def index
     @notes = @user.notes
   end
 
-  # GET /notes/1
-  # GET /notes/1.json
   def show
   end
 
-  # GET /notes/new
   def new
     @note = @user.notes.new
   end
 
-  # GET /notes/1/edit
   def edit
   end
 
-  # POST /notes
-  # POST /notes.json
   def create
     @note = @user.notes.build(note_params)
 
     respond_to do |format|
       if @note.save
         format.html { redirect_to [@user, @note], notice: 'Note was successfully created.' }
-        format.json { render :show, status: :created, location: [@user, @note] }
       else
         format.html { render :new }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /notes/1
-  # PATCH/PUT /notes/1.json
   def update
     respond_to do |format|
       if @note.update(note_params)
         format.html { redirect_to [@user, @note], notice: 'Note was successfully updated.' }
-        format.json { render :show, status: :ok, location: [@user, @note] }
       else
         format.html { render :edit }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /notes/1
-  # DELETE /notes/1.json
   def destroy
     @note.destroy
     respond_to do |format|
-      format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to user_notes_url(@user), notice: 'Note was successfully destroyed.' }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+
     def set_note
+      def set_user
+        @user = User.find(params[:user_id])
+      end
       @note = @user.notes.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def note_params
       params.require(:note).permit(:date, :content)
-    end
-
-    def set_user
-      @user = User.find(params[:user_id])
     end
 end
